b4f39eea5cbeec7c55d5a1072991a00e4fa6a1f4
Dolly-Mahour <dollymahour282@gmail.com>
2026-02-17 15:28:55 +0530

    Update homePage.js
    js navbar path

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("navbar.html")
+  fetch("./navbar.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("header").innerHTML = data;
